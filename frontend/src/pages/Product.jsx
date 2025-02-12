aa68cdc5ae34e3aaae7ee6e9d90233f19c467246
Prince294 <72266126+Prince294@users.noreply.github.com>
2025-02-12 09:35:22 +0530

    adding expandable description

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -99,7 +99,7 @@ const Product = () => {
               <p className='pl-2'>(122)</p>
           </div> */}
               <p className='mt-5 text-3xl font-medium'>{currency}{productData?.price}</p>
-              {<p className='text-sm text-secondary capitalize mt-2'>{productData?.description?.length < 250 || expand ? productData?.description : productData?.description.substring(0,250) + "..."}  <span className='text-blue-500 cursor-pointer hover:text-blue-700' onClick={()=> setExpand(prev => !prev)}>{productData?.description?.length > 250 && expand ? "Show Less" : "Show More"}</span></p>}
+              {<p className='text-sm text-secondary capitalize mt-2'>{productData?.description?.length < 250 || expand ? productData?.description : productData?.description.substring(0,250) + "..."}  {productData?.description?.length > 250 ? <span className='text-blue-500 cursor-pointer hover:text-blue-700' onClick={()=> setExpand(prev => !prev)}>{productData?.description?.length > 250 && expand ? "Show Less" : "Show More"}</span> : <></>}</p>}
 
 
               <div className='flex flex-col gap-4 my-6'>
